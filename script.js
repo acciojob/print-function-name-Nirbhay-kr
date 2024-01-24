e14490279235552276445281819720ca524613db
Nirbhay kumar <126154810+Nirbhay-kr@users.noreply.github.com>
2024-01-24 12:54:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,18 @@ function functionName() {
 }
 functionName();
 
+//when you call functionName(), it will trigger an alert with the message 
+//"functionName". You can use a similar structure
+//for other functions as well,
+//like the "AccioJob" function you mentioned:
+
 function Acciojob() {
 	alert("Acciojob");
 }
 Acciojob();
+
+//Both functions work by showing an alert with 
+//their respective names when called.
 
 
 
